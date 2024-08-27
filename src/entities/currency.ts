dd20aff8bf172f52721e374ea4deb6395e8df779
alienswap <alienswapz@protonmail.com>
2024-08-26 23:09:08 -0400

    Updated contracts

--- src/entities/currency.ts
+++ src/entities/currency.ts
@@ -16,7 +16,7 @@ export class Currency {
   /**
    * The only instance of the base class `Currency`.
    */
-  public static readonly ETHER: Currency = new Currency(18, 'XZNN', 'xZNN')
+  public static readonly ETHER: Currency = new Currency(18, 'xZNN', 'xZNN')
 
   /**
    * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ETHER`.
